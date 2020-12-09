d2b0caa794b23e022232f5a4cf86031b163f7ae9
Karen G <gks181188@yandex.ru>
2020-12-09 15:58:12 +0300

    done: popup, delete-icon

--- script/script.js
+++ script/script.js
@@ -1,40 +1,70 @@
 /*variables*/
-/*popup*/
+/*popup edit profile*/
+let popup = document.querySelector('.pop-up');
 let editButton = document.querySelector('.profile__edit-button');
 let closeBtn = document.querySelector('.pop-up__close-btn');
-let popup = document.querySelector('.pop-up');
-/*/popup*/
-/*form*/
+/*/popup edit profile*/
+
+/*popup add profile*/
+let popupImg = document.querySelector('.pop-up_img');
+let addButton = document.querySelector('.profile__add-button');
+let closeBtnImg = popupImg.querySelector('.pop-up__close-btn');
+/*/popup add profile*/
+
+/*form edit profile*/
 let formElement = document.querySelector('.pop-up__form');
 let nameInput = document.querySelector('.pop-up__input-text_type_name');
 let jobInput = document.querySelector('.pop-up__input-text_type_job');
 let name = document.querySelector('.profile__title');
 let job = document.querySelector('.profile__subtitile');
-/*form*/
+/*form edit profile*/
+
+/*form add profile*/
+//let formElementImg = popupImg.querySelector('.pop-up__form');
+/*form add profile*/
 /*/variables*/
 
-/*popup*/
-function openPopup() {
-  popup.classList.add('pop-up_opened');
+/*popup edit profile*/
+function openPopup(transmitted) {
+  transmitted.classList.add('pop-up_opened');
   nameInput.value = name.textContent;
   jobInput.value = job.textContent;
 }
 
-function closePopup() {
-  popup.classList.remove('pop-up_opened');
+function closePopup(transmitted) {
+  transmitted.classList.remove('pop-up_opened');
 }
-/*/popup*/
+/*/popup edit profile*/
 
-/*form*/
+/*form edit profile*/
 function formSubmitHandler(evt) {
   evt.preventDefault();
   name.textContent = nameInput.value;
   job.textContent = jobInput.value;
 
-  closePopup();
+  closePopup(popup);
+  //closePopup(popupImg);
 }
-/*/form*/
+/*/form edit profile*/
 
-editButton.addEventListener('click', openPopup);
-closeBtn.addEventListener('click', closePopup);
+/*popup add profile*/
+/*/popup add profile*/
+
+/*cards*/
+/*/cards*/
+
+editButton.addEventListener('click', () => {
+  openPopup(popup);
+});
+closeBtn.addEventListener('click', () => {
+  closePopup(popup);
+});
 formElement.addEventListener('submit', formSubmitHandler);
+
+addButton.addEventListener('click', () => {
+  openPopup(popupImg);
+});
+closeBtnImg.addEventListener('click', () => {
+  closePopup(popupImg);
+});
+//formElementImg.addEventListener('submit', formSubmitHandler);
